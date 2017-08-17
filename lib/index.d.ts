61609377f76dc598c4fc286182fa34b614441215
Joost Verdoorn <jpverdoorn@gmail.com>
2017-08-17 23:12:12 +0200

    Use typescript

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+declare const NAME: string, CAYLEY_ADDRESS: string, KAFKA_ADDRESS: string, INPUT_TOPIC: string, OUTPUT_TOPIC: string, CONCURRENCY: string | number;
+declare const memux: any;
+declare const Cayley: any;
+declare const empty: any;
+declare const PQueue: any;
+declare const source: any, sink: any, send: any;
+declare const cayley: any;
+declare const queue: any;
