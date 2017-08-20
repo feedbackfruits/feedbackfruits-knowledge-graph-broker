c89c1e9f29df223fe277fded9496116e90c69d16
Steffan Sluis <steffansluis@gmail.com>
2017-08-20 17:15:53 +0200

    Update memux

--- lib/jsonld.d.ts
+++
@@ -1,3 +0,0 @@
-export declare type JsonLd = object;
-export declare const isJsonLd: (jsonld: object) => boolean;
-export default JsonLd;
--- src/index.ts
+++ src/index.ts
@@ -1,95 +1,75 @@
 require('dotenv').load({ silent: true });
 
-const {
-  NAME = 'feedbackfruits-knowledge-graph-broker',
-  CAYLEY_ADDRESS = 'http://cayley:64210',
-  KAFKA_ADDRESS = 'tcp://kafka:9092',
-  INPUT_TOPIC = 'quad_update_requests',
-  OUTPUT_TOPIC = 'quad_updates',
-  CONCURRENCY = 100,
-} = process.env;
+import { createSend, createReceive, Operation, isOperation } from 'memux';
 
-// const memux = require('memux');
+import Doc from './doc';
+import { docToQuads, quadsToDocs } from './helpers';
+
 const Cayley = require('cayley');
 const { Observable: { empty } } = require('rxjs');
 const PQueue = require('p-queue');
 const jsonld = require('jsonld');
 
+const {
+  NAME = 'feedbackfruits-knowledge-graph-broker',
+  CAYLEY_ADDRESS = 'http://localhost:64210',
+  KAFKA_ADDRESS = 'tcp://localhost:9092',
+  INPUT_TOPIC = 'quad_update_requests',
+  OUTPUT_TOPIC = 'quad_updates',
+  CONCURRENCY = '100',
+} = process.env;
 
-import Doc from './doc';
-import { docToQuads, quadsToDocs } from './helpers';
+const cayley: any = null;
 
-type Operation = {
-  op: 'write' | 'delete'
-  doc: Doc
-};
-
-const isOperation = (operation: object): operation is Operation => {
-  typeof operation['quad'] === 'string' && operation['op'] === 'string';
-
-  return true;
-};
-
-type MemuxConfig = {
+type Config = {
   name: string,
   url: string,
   input: string,
   output: string,
-  receive: (operation: Operation) => Promise<void>
 };
 
-type Memux = (config: MemuxConfig) => {
-  send: (operation: Operation) => Promise<void>
-};
-
-const memux: Memux = require('memux');
-const cayley: any = null;
-
-// import memux from 'memux'
-// var x: Quad;
-// x.
-
-
-const receive = (operation: Operation) => {
-  if (!isOperation(operation)) throw new Error();
-  const { op, doc } = operation;
-
-  const quads = docToQuads(doc);
-  quads.forEach(quad => {
-    cayley[op](quad);
+async function init({ name, url, input, output }: Config) {
+  const send = await createSend({
+    name,
+    url,
+    topic: output,
+    concurrency: parseInt(CONCURRENCY)
   });
 
-  return Promise.all(quadsToDocs(cayley.get(doc['@id'])).map(doc => send({ doc, op }))).then(() => {});
-};
+  const receive = (operation: Operation<Doc>) => {
+    if (!isOperation(operation)) throw new Error();
+    const { action, data } = operation;
 
-const { send } = memux({
+    const quads = docToQuads(data);
+
+    return new Promise<void>((resolve, reject) => {
+      cayley[action](quads, (error, body, response) => {
+        if (error) return reject(error);
+        if (response.statusCode >= 200 && response.statusCode < 400) {
+          return Promise.all(quadsToDocs(cayley.get(data['@id'])).map(data => send({ action, key: data['@id'], data }))).then(() => resolve());
+        }
+
+        if (response.statusCode === 400) {
+          if ((body.error || body).match(/quad exists/)) return resolve();
+          if ((body.error || body).match(/invalid quad/)) return resolve();
+        }
+
+        reject();
+      });
+    });
+  };
+
+  return createReceive({
+    name,
+    url,
+    topic: input,
+    receive
+  });
+}
+
+init({
   name: NAME,
   url: KAFKA_ADDRESS,
   input: INPUT_TOPIC,
   output: OUTPUT_TOPIC,
-  receive
-});
-
-
-
-
-// const cayley = Cayley(CAYLEY_ADDRESS);
-// const queue = new PQueue({
-//   concurrency: CONCURRENCY
-// });
-
-// source.flatMap(({ action: { type, quad }, progress }) => {
-//   return queue.add(() => new Promise((resolve, reject) => {
-//     cayley[type]([quad], (error, body, response) => {
-//       if (error) return reject(error);
-//       if (response.statusCode >= 200 && response.statusCode < 400) return send({ type, quad }).then(resolve);
-//
-//       if (response.statusCode === 400) {
-//         if ((body.error || body).match(/quad exists/)) return resolve();
-//         if ((body.error || body).match(/invalid quad/)) return resolve();
-//       }
-//
-//       reject();
-//     });
-//   })).then(() => progress)
-// }).subscribe(sink);
+}).catch(console.error);
