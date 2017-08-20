c34d5780f91fafcdb1ec26671bde1b578e610297
Steffan Sluis <steffansluis@gmail.com>
2017-08-20 17:15:53 +0200

    Update memux

--- test/index.js
+++ test/index.js
@@ -1,11 +1,7 @@
 import test from 'ava';
-import { quadsToJsonLD } from '../lib'
+import { docToQuads, quadsToDocs } from '../lib/helpers'
 
-test('stub', t => {
-  t.is(true, true);
-});
-
-test('quadsToJsonLD', t => {
+test('quadsToDocs', t => {
   const quads = [
     { subject: '<http://some.domain/janedoe>', predicate: '<http://schema.org/jobTitle>', object: '"Professor"'},
     { subject: '<http://some.domain/janedoe>', predicate: '<http://schema.org/name>', object: '"Jane Doe"'},
@@ -14,15 +10,16 @@ test('quadsToJsonLD', t => {
     { subject: '<http://some.domain/janedoe>', predicate: '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>', object: '<http://schema.org/Person>'},
   ];
 
-  const result = {
-    '<http://some.domain/janedoe>': {
-      '<http://schema.org/jobTitle>': [ '"Professor"' ],
-      '<http://schema.org/name>': [ '"Jane Doe"' ],
-      '<http://schema.org/telephone>': [ '"(425) 123-4567"' ],
-      '<http://schema.org/url>': [ '<http://www.janedoe.com>' ],
-      '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>': [ '<http://schema.org/Person>' ]
-    }
-  };
+  const result = [
+    {
+     '<http://schema.org/jobTitle>': [ '"Professor"' ],
+     '<http://schema.org/name>': [ '"Jane Doe"' ],
+     '<http://schema.org/telephone>': [ '"(425) 123-4567"' ],
+     '<http://schema.org/url>': [ '<http://www.janedoe.com>' ],
+     '<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>': [ '<http://schema.org/Person>' ],
+     '@id': '<http://some.domain/janedoe>',
+    },
+  ];
 
-  t.deepEqual(quadsToJsonLD(quads), result);
+  t.deepEqual(quadsToDocs(quads), result);
 })
