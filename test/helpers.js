a7b9e462d2356b217140fe7c98ab33fa7c2c2483
Steffan Sluis <steffansluis@gmail.com>
2018-04-18 23:44:04 +0200

    Deduplicate existing quads

---
+++ test/helpers.js
@@ -0,0 +1,21 @@
+import test from 'ava';
+
+import * as Helpers from '../lib/helpers';
+
+
+test('it exists', t => {
+  t.not(Helpers, undefined);
+});
+
+test('Helpers.deduplicateQuads: it returns deduplicated quads', async t => {
+  const quad = {
+    "subject": "https://video.google.com/timedtext?v=2Op3QLzMgSY&lang=en#UDM0MTUuMTlTLVAxLjdT",
+    "predicate": "http://schema.org/text",
+    "object": "\"you know that these languages have things like \"for\"",
+    "label": ""
+  };
+
+  const quads = [ quad, quad, quad ];
+  const res = Helpers.deduplicateQuads(quads);
+  return t.deepEqual(res, [ quad ]);
+});
