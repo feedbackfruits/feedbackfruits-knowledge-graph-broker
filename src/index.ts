e3a25bf066775e2b8b9c8107a5aa3050bd724986
Steffan Sluis <steffansluis@gmail.com>
2018-04-18 17:50:41 +0200

    Don't trace broken quads anymore

--- src/index.ts
+++ src/index.ts
@@ -57,19 +57,19 @@ async function init({ name }: BrokerConfig) {
       console.log('ERROR! Skipping doc.');
       console.error(e)
 
-      console.log('Tracing broken quad:');
-      let quads;
-      quads = await Doc.toQuads(data);
-      await Promise.all(quads.map(async quad => {
-        let nquads, doc;
-        try {
-          nquads = Quad.toNQuads([ quad ]);
-          quads = Quad.fromNQuads(nquads);
-          doc = await Doc.fromQuads(quads, Context.context);
-        } catch(e) {
-          console.log('Errored on quad:', JSON.stringify(quad));
-        }
-      }));
+      // console.log('Tracing broken quad:');
+      // let quads;
+      // quads = await Doc.toQuads(data);
+      // await Promise.all(quads.map(async quad => {
+      //   let nquads, doc;
+      //   try {
+      //     nquads = Quad.toNQuads([ quad ]);
+      //     quads = Quad.fromNQuads(nquads);
+      //     doc = await Doc.fromQuads(quads, Context.context);
+      //   } catch(e) {
+      //     console.log('Errored on quad:', JSON.stringify(quad));
+      //   }
+      // }));
 
       return;
     }
