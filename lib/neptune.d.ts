8ec6105573d4010ecd96e3ed04f00456a60a25d4
Steffan Sluis <steffansluis@gmail.com>
2018-07-10 01:30:53 +0200

    Make use of poseidon for testing

--- lib/neptune.d.ts
+++ lib/neptune.d.ts
@@ -1,7 +1,9 @@
 import { Quad } from 'feedbackfruits-knowledge-engine';
 export declare function parseResult(result: any): any;
 export declare function queryNeptune(query: string): Promise<any>;
-export declare function updateNeptune(update: string): Promise<any>;
+export declare function updateNeptune(update: string): Promise<{
+    update: boolean;
+}>;
 export declare function getQuads(...subjects: string[]): Promise<any>;
 export declare function writeQuads(quads: Quad[]): Promise<void>;
 export declare function deleteQuads(quads: Quad[]): Promise<void>;
