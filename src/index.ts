32c42a884c66fd24d9c3fff92cd32bbf6e035ad8
Steffan Sluis <steffansluis@gmail.com>
2019-07-10 17:16:22 +0200

    Run yarn upgrade --latest and do some cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,15 @@
 import { Operation, isOperation } from 'memux';
 import { Annotator, Doc, Quad, Context } from 'feedbackfruits-knowledge-engine';
 
-const Cayley = require('cayley');
-const jsonld = require('jsonld');
-import { unionBy, differenceBy } from 'lodash';
-
 import * as Config from './config';
 import * as Helpers from './helpers';
 import * as Neptune from './neptune';
-
-const cayley = Cayley(Config.CAYLEY_ADDRESS);
 
 export type BrokerConfig = {
   name: string,
 };
 
 export type SendFn = (operation: Operation<Doc>) => Promise<void>;
-
-// async function traceExistingQuads(quads: Quad[]): Promise<Quad[]> {
-//   return quads.reduce(async (memo, quad) => {
-//     const exists = await Helpers.quadExists(quad);
-//     if (exists) return [ ...(await memo), quad];
-//     return memo;
-//   }, Promise.resolve(<Quad[]>[]));
-// }
-
-function quadIdentity(quad) {
-  // return JSON.stringify(quad);
-  const { subject, predicate, object, label } = quad;
-  return Quad.toNQuads([ quad ]);
-  // return `${label}: ${subject} ${predicate} ${object}`;
-}
 
 async function init({ name }: BrokerConfig) {
   const receive = (send: SendFn) => async (operation: Operation<Doc>) => {
@@ -76,24 +55,8 @@ async function init({ name }: BrokerConfig) {
       await send({ action, key: framed['@id'], data: framed });
     }));
 
-    // const diffLength = diffLengths.reduce((memo, length) => memo + (length || 0), 0);
-    // console.log('Total diffLength:', diffLength);
-      // await Promise.all(docs.map(data => send({ action, key: data['@id'], data })));
-        // console.log('Tracing duplicate quad:');
-      // let quads;
-      // await Promise.all(quads.map(async quad => {
-      //   const exists = await Helpers.quadExists(quad);
-      //   if (exists) {
-      //     console.log('Errored on quad:', JSON.stringify(quad));
-      //     process.exit(1);
-      //     // throw new Error('Existing quad: ' + JSON.stringify(quad));
-      //   }
-      // }));
-
-      return;
-    }
-
-  // };
+    return;
+  }
 
   return Annotator({
     name,
